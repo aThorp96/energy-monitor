52ef6d6b907fde98e91d9498cf92bbb4d031a458
Andrew Thorp <andrew.thorp.dev@gmail.com>
2024-01-17 22:03:52 -0500

    remove emonlib fork and just send raw readings

--- energy_monitor.ino
+++ energy_monitor.ino
@@ -1,24 +1,29 @@
 // Include Emon Library
-#include "./EmonLib.h"
+#include "EmonLib.h"
 
 // Create an instance
 EnergyMonitor emon1;
+int lastSend = 0;
+int now = 0;
 
+#define IPIN 1
+#define VPIN 2
 
 void setup(){
     Serial.begin(115200);
 
-    emon1.current(1, 111.1);
-
-    while(!emon1.ready()) {
-        emon1.loop();
-    }
+    Serial.println("VREAD,IREAD,VCC");
 }
 
+
 void loop() {
-    emon1.loop();
-    double Irms = emon1.calcIrms();  // Calculate Irms only
-    //Serial.print(Irms*123.0);         // Apparent power
-    //Serial.print(" ");
-    Serial.println(Irms * 123);          // Irms
+    now = millis();
+    if (lastSend < now) {
+        lastSend = now;
+        Serial.print(analogRead(VPIN));
+        Serial.print(',');
+        Serial.print(analogRead(IPIN));
+        Serial.print(',');
+        Serial.println(emon1.readVcc());
+    }
 }
